61bd5ccf5030535d0ded6fa916e6f193cd75a582
Joris De Winne <joris.dewinne@gmail.com>
2017-11-08 21:22:57 -0800

    Adding analyzer functionality

--- src/main/jython/delivery/fetch_data.py
+++ src/main/jython/delivery/fetch_data.py
@@ -1,131 +1,92 @@
-from com.xebialabs.deployit.exception import NotFoundException
+class Marker(object):
+    def __init__(self, environment, component, start, stop, conflict):
+        self.environment = environment
+        self.component = component
+        self.start = start
+        self.stop = stop
+        self.conflict = conflict
+
+    def matches(self, environment, component):
+        return environment == self.environment and component == self.component
 
 
-class Node(object):
-    def __init__(self, name, node_id, kind, status):
-        self.name = name
-        self.id = node_id
-        self.kind = kind
-        self.status = status
-
-
-class Edge(object):
-    def __init__(self, source_id, target_id, name, curve=0):
-        self.source = source_id
-        self.target = target_id
-        self.name = name
-        self.curve = curve
-        self.cardinality = 1
-
-
-class Graph(object):
+class Conflicts(object):
     def __init__(self):
-        self.processed_nodes = []
-        self.nodes = []
-        self.edges = []
+        self.markers = {}
 
-    def add_edge(self, source_id, target_id, task_name):
-        for e in self.edges:
-            if e.source == source_id and e.target == target_id:
-                e.cardinality += 1
-                e.name = "%s, %s" % (e.name, task_name)
+    def add_marker(self, release_id, environment, component, start_date):
+        marker = Marker(environment,component,start_date, None, False)
+        if release_id in self.markers:
+            self.markers[release_id].append(marker)
+        else:
+            self.markers[release_id] = [marker]
+
+
+    def add_unmarker(self, release_id, environment, component, end_date):
+        if release_id not in self.markers:
+            return
+        for marker in self.markers[release_id]:
+            if marker.matches(environment, component) and marker.stop is None:
+                marker.stop = end_date
                 return
 
-        reverse_edge = None
-        curve = 0
-        for e in self.edges:
-            if e.source == target_id and e.target == source_id:
-                reverse_edge = e
-        if reverse_edge is not None:
-            curve = reverse_edge.curve + 1
+    def date_overlap(self, marker, o_marker):
+        Range = namedtuple('Range', ['start', 'end'])
+        r1 = Range(start=marker.start, end=marker.stop)
+        r2 = Range(start=o_marker.start, end=o_marker.end)
+        latest_start = max(r1.start, r2.start)
+        earliest_end = min(r1.end, r2.end)
+        overlap = (earliest_end - latest_start).days + 1
+        return overlap > 0
 
-        self.edges.append(Edge(source_id, target_id, task_name, curve))
+    def check_overlap(self, marker):
+        for release_id, markers in self.markers.items():
+            for o_marker in markers:
+                if marker != o_marker and o_marker.environment == marker.environment and self.date_overlap(marker, o_marker):
+                    return True
 
-    def add_node(self, name, node_id, kind, status):
-        node = Node(name, node_id, kind, status)
-        self.nodes.append(node)
-        self.processed_nodes.append(node_id)
-        return node
 
-    def node_processed(self, nid):
-        return nid in self.processed_nodes
+    def analyze_conflicts(self):
+        for release_id, markers in self.markers.items():
+            for marker in markers:
+                if self.check_overlap(marker):
+                    marker.conflict = True
 
     def to_dict(self):
-        result_dict = {"nodes": [], "edges": []}
-        for n in self.nodes:
-            result_dict["nodes"].append({"name": n.id, "displayLabel": n.name, "kind": n.kind, "status": str(n.status)})
-        for e in self.edges:
-            result_dict["edges"].append(
-                {"source": e.source, "target": e.target, "displayLabel": e.name, "curve": e.curve,
-                 "cardinality": e.cardinality})
+        result_dict = []
+        for release_id, markers in self.markers.items():
+            for marker in markers:
+                result_dict.append({"release": release_id, "environment": marker.environment, "component": marker.component, "start": str(marker.start), "stop": str(marker.stop), "conflict": marker.conflict})
         return result_dict
 
 
-def resolve_gate_release_id(plan_item_id):
-    release_id_parts = []
-    for p in plan_item_id.split("/"):
-        if p.startswith("Phase"):
-            break
-        release_id_parts.append(p)
-    return "/".join(release_id_parts)
-
-
-def process_task(task, graph, node):
+def process_task(release_id, task, conflicts):
     if task.type in ["xlrelease.ParallelGroup", "xlrelease.SequentialGroup"]:
-        process_tasks(task.tasks, graph, node)
-    elif task.type == "xlrelease.CreateReleaseTask":
-        if str(task.status) == "COMPLETED":
-            graph.add_edge(node.id, task.createdReleaseId, task.title)
-            analyse(task.createdReleaseId, graph)
-        elif str(task.status) == "PLANNED":
-            graph.add_edge(node.id, task.templateId, task.title)
-            analyse(task.templateId, graph)
-    elif task.type == "xlrelease.GateTask" and len(task.dependencies) > 0:
-        for dep in task.dependencies:
-            if dep.hasResolvedTarget():
-                if dep.target is not None and dep.target.id is not None:
-                    target_id = dep.target.id
-                elif dep.targetId is not None:
-                    target_id = dep.targetId
-                else:
-                    continue
-                target_id = resolve_gate_release_id(target_id)
-                graph.add_edge(node.id, target_id, task.title)
-                analyse(target_id, graph)
+        process_tasks(release_id, task.tasks, conflicts)
+    elif task.type == "xlrelease.CustomScriptTask" and task.pythonScript.type == "delivery.mark":
+        conflicts.add_marker(release_id, task.pythonScript.getProperty("environment"),
+                             task.pythonScript.getProperty("component"), task.scheduledStartDate)
+    elif task.type == "xlrelease.CustomScriptTask" and task.pythonScript.type == "delivery.unmark":
+        conflicts.add_unmarker(release_id, task.pythonScript.getProperty("environment"),
+                               task.pythonScript.getProperty("component"), task.dueDate)
 
 
-def process_tasks(tasks, graph, node):
-    [process_task(t, graph, node) for t in tasks]
+def process_tasks(release_id, tasks, conflicts):
+    [process_task(release_id, t, conflicts) for t in tasks]
 
 
-def analyse(release_id, graph):
-    if not graph.node_processed(release_id):
-        release = read(release_id)
-        if release is not None:
-            kind = "template" if str(release.status) == "TEMPLATE" else "release"
-            node = graph.add_node(release.title, release_id, kind, release.status)
-            [process_tasks(p.tasks, graph, node) for p in release.phases]
+def process_release(release, conflicts):
+    [process_tasks(release.id, p.tasks, conflicts) for p in release.phases]
 
 
-def read(release_id):
-    try:
-        return templateApi.getTemplate(release_id)
-    except NotFoundException:
-        try:
-            return releaseApi.getArchivedRelease(release_id)
-        except NotFoundException:
-            msg = "Release id [%s] not found. " % release_id
-            msg += "Could be caused by the importing a template that has a reference to a non-existing template. "
-            msg += "Another cause could could be the removal of archived releases."
-            logger.error(msg)
-            return None
+def analyse(conflicts):
+    releases = releaseApi.getReleases()
+    [process_release(release, conflicts) for release in releases]
 
 
-# rid = request.query["id"]
-# rid = rid.replace("-", "/")
-# result_graph = Graph()
-# analyse(rid, result_graph)
+result_conflicts = Conflicts()
+analyse(result_conflicts)
+result_conflicts.analyze_conflicts()
 
-response.entity = [
-    {"environment": "env1", "component": "abc", "start": "11-06-2017", "end": "11-10-2017", "conflict": "true"},
-    {"environment": "env2", "component": "abc", "start": "11-06-2017", "end": "11-11-2017", "conflict": "false"}]
+
+response.entity = result_conflicts.to_dict()
